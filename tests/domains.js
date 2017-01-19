2d43953c23d813ed9602efb8272b246aa7275652
Fredrik Holmén <fredrik.holmen@feedeo.com>
2017-01-19 18:25:44 +0100

    Added test cases for Australia

--- tests/domains.js
+++ tests/domains.js
@@ -15,6 +15,19 @@ describe('root-extract', function() {
     expect(rooter.rootDomain("http://www.example.edu.ac").rootDomain).to.equal("example.edu.ac");
     expect(rooter.rootDomain("http://www.example.edu.ac:8000").rootDomain).to.equal("example.edu.ac");
     expect(rooter.rootDomain("http://www.example.edu.ac").rootDomain).to.equal("example.edu.ac");
+    
+    //Australia
+expect(rooter.rootDomain("www.example.com.au").rootDomain).to.equal("example.com.au");
+ expect(rooter.rootDomain("www.example.net.au").rootDomain).to.equal("example.net.au");
+ expect(rooter.rootDomain("www.example.org.au").rootDomain).to.equal("example.org.au");
+ expect(rooter.rootDomain("www.example.edu.au").rootDomain).to.equal("example.edu.au");
+ expect(rooter.rootDomain("www.example.gov.au").rootDomain).to.equal("example.gov.au");
+ expect(rooter.rootDomain("www.example.asn.au").rootDomain).to.equal("example.asn.au");
+ expect(rooter.rootDomain("www.example.id.au").rootDomain).to.equal("example.id.au");
+ expect(rooter.rootDomain("www.example.csiro.au").rootDomain).to.equal("example.csiro.au");
+ expect(rooter.rootDomain("www.example.oz.au").rootDomain).to.equal("example.oz.au");
+ expect(rooter.rootDomain("www.example.conf.au").rootDomain).to.equal("example.conf.au");
+ expect(rooter.rootDomain("www.example.info.au").rootDomain).to.equal("example.info.au");
    
   });
 });
