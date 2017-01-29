2af254aa18506d1c00d35316c7986e68485d78b2
Fredrik Holmen <fredrik.holmen@feedeo.com>
2017-01-29 15:12:40 +0100

    Renamed to rootExtract.js through-out and changed name of .rootDomain() function to .parse().

--- README.md
+++ README.md
@@ -1,21 +1,24 @@
 # rootExtract.js
 Simple package to extract the root domain from an URL
 
+rootExtract.js takes a URL string parses it and returns a URL object with a rootDomain field.
+
 ## Install
 
 ```
-	npm install root-extract.js
+	npm install rootExtract.js
 ```
 
 ## Usage
 
 ```
-	const root = require('root-extract.js')
+	const rooter = require('rootExtract.js')
 
-	let link = "www.example.co.uk"
+	let link = "http://www.example.co.uk"
 
-	let URL = root.rootDomain(link)
+	let URL = rooter.parse(link)
 
+	//We return the normal URL object with an appended rootDomain field.
 	console.log(URL.rootDomain)
 
 	//Prints out 'example.co.uk'
--- tests/domains.js
+++ tests/domains.js
@@ -1,41 +1,42 @@
 var chai = require('chai');
 var expect = chai.expect; // we are using the "expect" style of Chai
 
-var rooter = require('./../lib/root-extract');
+var rooter = require('./../lib/rootExtract');
 
-describe('root-extract', function() {
-  it('rootDomain() should return the correct root domain from any url passed in', function() {
+
+describe('rootExtract', function() {
+  it('parse() should return the correct root domain from any url passed in', function() {
     
-    expect(rooter.rootDomain("www.example.com").rootDomain).to.equal("example.com");
-    expect(rooter.rootDomain("example.com").rootDomain).to.equal("example.com");
-    expect(rooter.rootDomain("www.example.co.uk").rootDomain).to.equal("example.co.uk");
-    expect(rooter.rootDomain("www.example.org.uk").rootDomain).to.equal("example.org.uk");
-    expect(rooter.rootDomain("www.example.mil.ac").rootDomain).to.equal("example.mil.ac");
-    expect(rooter.rootDomain("www.example.edu.ac").rootDomain).to.equal("example.edu.ac");
-    expect(rooter.rootDomain("http://www.example.edu.ac").rootDomain).to.equal("example.edu.ac");
-    expect(rooter.rootDomain("http://www.example.edu.ac:8000").rootDomain).to.equal("example.edu.ac");
-    expect(rooter.rootDomain("http://www.example.edu.ac").rootDomain).to.equal("example.edu.ac");
+    expect(rooter.parse("http://www.example.com").rootDomain).to.equal("example.com");
+    expect(rooter.parse("http://example.com").rootDomain).to.equal("example.com");
+    expect(rooter.parse("ftp://www.example.co.uk").rootDomain).to.equal("example.co.uk");
+    expect(rooter.parse("http://www.example.org.uk").rootDomain).to.equal("example.org.uk");
+    expect(rooter.parse("http://www.example.mil.ac").rootDomain).to.equal("example.mil.ac");
+    expect(rooter.parse("http://www.example.edu.ac").rootDomain).to.equal("example.edu.ac");
+    expect(rooter.parse("http://www.example.edu.ac").rootDomain).to.equal("example.edu.ac");
+    expect(rooter.parse("http://www.example.edu.ac:8000").rootDomain).to.equal("example.edu.ac");
+    expect(rooter.parse("http://www.example.edu.ac").rootDomain).to.equal("example.edu.ac");
     
     //Australia
-    expect(rooter.rootDomain("www.example.com.au").rootDomain).to.equal("example.com.au");
-    expect(rooter.rootDomain("www.example.net.au").rootDomain).to.equal("example.net.au");
-    expect(rooter.rootDomain("www.example.org.au").rootDomain).to.equal("example.org.au");
-    expect(rooter.rootDomain("www.example.edu.au").rootDomain).to.equal("example.edu.au");
-    expect(rooter.rootDomain("www.example.gov.au").rootDomain).to.equal("example.gov.au");
-    expect(rooter.rootDomain("www.example.asn.au").rootDomain).to.equal("example.asn.au");
-    expect(rooter.rootDomain("www.example.id.au").rootDomain).to.equal("example.id.au");
-    expect(rooter.rootDomain("www.example.csiro.au").rootDomain).to.equal("example.csiro.au");
-    expect(rooter.rootDomain("www.example.oz.au").rootDomain).to.equal("example.oz.au");
-    expect(rooter.rootDomain("www.example.conf.au").rootDomain).to.equal("example.conf.au");
-    expect(rooter.rootDomain("www.example.info.au").rootDomain).to.equal("example.info.au");
+    expect(rooter.parse("http://www.example.com.au").rootDomain).to.equal("example.com.au");
+    expect(rooter.parse("http://www.example.net.au").rootDomain).to.equal("example.net.au");
+    expect(rooter.parse("http://www.example.org.au").rootDomain).to.equal("example.org.au");
+    expect(rooter.parse("http://www.example.edu.au").rootDomain).to.equal("example.edu.au");
+    expect(rooter.parse("http://www.example.gov.au").rootDomain).to.equal("example.gov.au");
+    expect(rooter.parse("http://www.example.asn.au").rootDomain).to.equal("example.asn.au");
+    expect(rooter.parse("http://www.example.id.au").rootDomain).to.equal("example.id.au");
+    expect(rooter.parse("http://www.example.csiro.au").rootDomain).to.equal("example.csiro.au");
+    expect(rooter.parse("http://www.example.oz.au").rootDomain).to.equal("example.oz.au");
+    expect(rooter.parse("http://www.example.conf.au").rootDomain).to.equal("example.conf.au");
+    expect(rooter.parse("http://www.example.info.au").rootDomain).to.equal("example.info.au");
 
-    //Mexico
-    expect(rooter.rootDomain("www.example.com.mx").rootDomain).to.equal("example.com.mx");
-    expect(rooter.rootDomain("www.example.net.mx").rootDomain).to.equal("example.net.mx");
-    expect(rooter.rootDomain("www.example.org.mx").rootDomain).to.equal("example.org.mx");
-    expect(rooter.rootDomain("www.example.ngo.mx").rootDomain).to.equal("example.ngo.mx");
-    expect(rooter.rootDomain("www.example.edu.mx").rootDomain).to.equal("example.edu.mx");
-    expect(rooter.rootDomain("www.example.gob.mx").rootDomain).to.equal("example.gob.mx");
+    //Mexico    
+    expect(rooter.parse("http://www.example.com.mx").rootDomain).to.equal("example.com.mx");
+    expect(rooter.parse("http://www.example.net.mx").rootDomain).to.equal("example.net.mx");
+    expect(rooter.parse("http://www.example.org.mx").rootDomain).to.equal("example.org.mx");
+    expect(rooter.parse("http://www.example.ngo.mx").rootDomain).to.equal("example.ngo.mx");
+    expect(rooter.parse("http://www.example.edu.mx").rootDomain).to.equal("example.edu.mx");
+    expect(rooter.parse("http://www.example.gob.mx").rootDomain).to.equal("example.gob.mx");
    
 });
 });
